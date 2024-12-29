91a340d7c3b48c597b2417c38d01b7e85511832a
FarshadFahimi <FarshadFahimi@hotmail.com>
2024-12-29 13:38:55 +0330

    Some changes

--- Commands/Message Brokers/Run-RabbitMq-Container.bat
+++ Commands/Message Brokers/Run-RabbitMq-Container.bat
@@ -1,1 +1,1 @@
-docker run -d --name test-rabit -e RABBITMQ_DEFAULT_USER=guest -e RABBITMQ_DEFAULT_PASS=guest -p 5672:5672 -p 15672:15672 -t rabbitmq:3.13.1-management
+docker run -d --name test-rabit -e RABBITMQ_DEFAULT_USER=guest -e RABBITMQ_DEFAULT_PASS=guest -p 5672:5672 -p 15672:15672 -t rabbitmq:3.13.4-management
